603b527a435f5bc5933c89668d5a4c7663162b4a
kim-hyunjin <hyunjin1612@gmail.com>
2025-04-12 13:58:23 +0900

    json decoding

--- mobile/ios/udemy/Clima-iOS13-master/Clima/WeatherManager.swift
+++ mobile/ios/udemy/Clima-iOS13-master/Clima/WeatherManager.swift
@@ -22,27 +22,36 @@ struct WeatherManager {
             // 2. Create a URLSession
             let session = URLSession(configuration: .default)
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(safeData)
+                }
+            }
             // 4. Start the task
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(_ weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
             
-
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+        } catch {
+            print(error)
         }
-
-    }
         
-
+    }
+    
+    
 
 }
