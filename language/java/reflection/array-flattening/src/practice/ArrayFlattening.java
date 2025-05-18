ecb71fb9a9807fff8162a6e3c1b9ed535d91cda4
kim-hyunjin <hyunjin1612@gmail.com>
2025-05-18 14:47:55 +0900

    필드 값 설정하기, 배열 생성 및 초기화. 설정 파일 읽어 pojo 만들기

---
+++ language/java/reflection/array-flattening/src/practice/ArrayFlattening.java
@@ -0,0 +1,41 @@
+package practice;
+
+import java.util.*;
+import java.lang.reflect.*;
+
+public class ArrayFlattening {
+
+    public static void main(String[] args) {
+        ArrayFlattening arrayFlattening = new ArrayFlattening();
+        String[] result = arrayFlattening.concat(String.class, new String[] { "Hello", "World" },
+                new String[] { "Java", "Reflection" });
+        System.out.println(Arrays.toString(result));
+    }
+
+    public <T> T concat(Class<?> type, Object... arguments) {
+        if (arguments.length == 0) {
+            return null;
+        }
+
+        List<Object> elements = new ArrayList<>();
+        for (Object argument : arguments) {
+            if (argument.getClass().isArray()) {
+                int length = Array.getLength(argument);
+
+                for (int i = 0; i < length; i++) {
+                    elements.add(Array.get(argument, i));
+                }
+            } else {
+                elements.add(argument);
+            }
+        }
+
+        Object flattenedArray = Array.newInstance(type, elements.size());
+
+        for (int i = 0; i < elements.size(); i++) {
+            Array.set(flattenedArray, i, elements.get(i));
+        }
+
+        return (T) flattenedArray;
+    }
+}
